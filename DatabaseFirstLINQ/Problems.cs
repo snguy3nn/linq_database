8eb6ec1327ef340e18999f5c096fc855ab9f493f
Stephen Nguyen <snguy3nn@gmail.com>
2021-07-12 22:29:59 -0500

    ProblemThree completed

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -17,7 +17,7 @@ namespace DatabaseFirstLINQ
         {
             ProblemOne();
             ProblemTwo();
-            //ProblemThree();
+            ProblemThree();
             //ProblemFour();
             //ProblemFive();
             //ProblemSix();
@@ -47,6 +47,7 @@ namespace DatabaseFirstLINQ
             {
                 Console.WriteLine(users);
             }
+           
         }
 
         private void ProblemTwo()
@@ -65,7 +66,14 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets each product where the products price is greater than $150.
             // Then print the name and price of each product from the above query to the console.
+            var products = _context.Products;
+            var greaterThanOneFifty = products.Where(p => p.Price > 150);
 
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name + " " + product.Price);
+            }
+            
         }
 
         private void ProblemFour()
